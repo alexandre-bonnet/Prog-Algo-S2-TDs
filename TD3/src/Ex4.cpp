e637a22b7a74e870e9b5058f1036b9428d953053
Alexandre Bonnet <alexandre.bonnet@edu.esiee.fr>
2026-03-30 17:26:36 +0200

    Optimized Ex4

--- TD3/src/Ex4.cpp
+++ TD3/src/Ex4.cpp
@@ -6,19 +6,18 @@ bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(),
 
 int search(std::vector<int> const& vec, int const num, size_t const left, size_t const right){
     size_t middle = left + (right - left) / 2;
-    int indice = -1;
     if(num<vec[middle]){
-        indice = search(vec,num,left,middle-1);
+        return search(vec,num,left,middle-1);
     } else if(num>vec[middle]){
-        indice = search(vec,num,middle+1,right);
+        return search(vec,num,middle+1,right);
     } else if(num==vec[middle]){
         return middle;
-    } return indice ;
+    } return -1;
 }
 
 int main(){
     std::vector<int> array {1,2,3,4,5,6,7,8,9};
-    int num = 9;
+    int num = 5;
     if (is_sorted(array)) {
         std::cout << "Le nombre "<< num << " est a l'indice "<< search(array, num, 0, array.size()-1);
     }
